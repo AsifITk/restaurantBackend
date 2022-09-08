8d4752c9aba32ae4765e428f5689a95d05a38f7a
AsifITk <asifkhanit10@gmail.com>
2022-09-08 17:46:22 +0600

    final

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
 const Schema = new mongoose.Schema({
+    restaurant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Restaurant",
+        required: true,
+    }
     items: [],
     totalPrice: { type: Number, required: true },
     status: { type: String, required: true },
